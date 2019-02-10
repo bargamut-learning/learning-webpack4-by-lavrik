190928271864b9858b5c1c41920040149edc113c
Paul Petrov <paul@bargamut.ru>
2019-02-10 04:02:49 +0300

    Add simple .babelrc config
    Turn off using jQuery

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import $ from 'jquery';
+// import $ from 'jquery';
 import some from './some2';
 
-$('.title').html('Some text!');
+// $('.title').html('Some text!');
 
 console.log(some.avg(1, 4, 8));
 console.log(some.max(1, 4, 8));
