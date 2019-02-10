9f8be6e869bbf493292fcf4811e558576080ca4c
Paul Petrov <paul@bargamut.ru>
2019-02-10 03:47:27 +0300

    Add src/some2.js with latest JS standarts
    This will be brken in IE

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import $ from 'jquery';
-import avg from './some';
+import some from './some2';
 
 $('.title').html('Some text!');
 
-console.log(avg(1, 4, 8));
+console.log(some.avg(1, 4, 8));
+console.log(some.max(1, 4, 8));
+console.log(
+	some.merge(
+		{
+			a: 1
+		}, {
+			a: 'a',
+			b: 2
+		}
+	)
+);
