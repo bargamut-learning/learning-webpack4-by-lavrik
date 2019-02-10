9eb01b84ec1b1ec2262f974efca0a7046f534d3d
Paul Petrov <paul@bargamut.ru>
2019-02-10 03:14:57 +0300

    Fix mistake with output path and build
    Also I've installed path module for this

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
+let path = require('path');
+
 // Create some object: this will be configuration of Webpack 4
 let conf = {
 	// Source entry point
 	entry: './src/index.js',
 	// Output point
 	output: {
-		path: './dist/',
+		// Must be absolute path; may be with __dirname, concatenation or you can use "path" module
+		path: path.resolve(__dirname, './dist'),
 		filename: 'main.js'
 	}
 };
