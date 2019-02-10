c13079fc0d54b38612acd0577e5eff206f2826dd
Paul Petrov <paul@bargamut.ru>
2019-02-10 03:05:16 +0300

    Make some options to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,12 @@
 // Create some object: this will be configuration of Webpack 4
 let conf = {
-
+	// Source entry point
+	entry: './src/index.js',
+	// Output point
+	output: {
+		path: './dist/',
+		filename: 'main.js'
+	}
 };
 
 module.exports = conf;
